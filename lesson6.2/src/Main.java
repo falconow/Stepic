0561b826932a59e61f5f049fc6c36ed9bc548f48
Dmitry Sokolov <dmitriy88.rus@gmail.com>
2022-01-13 16:28:36 +0300

    add lesson 6.2

---
+++ lesson6.2/src/Main.java
@@ -0,0 +1,16 @@
+import com.sun.istack.internal.NotNull;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+        //ArrayList<T> arr = set1.toArray();
+        return null;
+    }
+}
