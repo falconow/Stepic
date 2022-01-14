c9b5802d704bd26e5c3c9be6a1781bf1cb3b763a
Dmitry Sokolov <dmitriy88.rus@gmail.com>
2022-01-14 14:49:50 +0300

    add solution lesson6.3

---
+++ lesson6.3/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Objects;
+import java.util.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+
+    }
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+
+        Function<T,U> result = x-> {
+            return condition.test(x) ? (U) ifTrue.apply(x): (U) ifFalse.apply(x);
+        };
+
+        return result;
+
+    }
+}
