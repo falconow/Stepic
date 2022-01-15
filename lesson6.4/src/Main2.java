7f95e55002122b66370df2a7b8c97c00cd5b7dcb
Dmitry Sokolov <dmitriy88.rus@gmail.com>
2022-01-15 23:48:35 +0300

    change lesson 6.4

---
+++ lesson6.4/src/Main2.java
@@ -0,0 +1,34 @@
+import jdk.nashorn.internal.runtime.regexp.joni.encoding.CharacterType;
+
+import javax.xml.stream.events.Characters;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main2 {
+
+    public static void main(String[] args) throws IOException {
+        Map<String,Integer> map = new HashMap<>();
+        Reader reader = new InputStreamReader(System.in,StandardCharsets.UTF_8);
+        Scanner scanner = new Scanner(reader).useDelimiter("[\\p{Space}\\p{Punct}]");
+
+        while (scanner.hasNext()) {
+            String element = scanner.next();
+            int count = map.containsKey(element) ? map.get(element)+1:1;
+            map.put(element,count);
+        }
+        scanner.close();
+
+        System.out.println(map);
+
+
+
+
+    }
+}
