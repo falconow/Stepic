f66651312bf47b234e69618a13e7133a6b0b7fb5
Dmitry Sokolov <dmitriy88.rus@gmail.com>
2022-01-16 22:44:25 +0300

    change lesson 6.4

--- lesson6.4/src/Main2.java
+++ lesson6.4/src/Main2.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -16,16 +17,29 @@ public class Main2 {
     public static void main(String[] args) throws IOException {
         Map<String,Integer> map = new HashMap<>();
         Reader reader = new InputStreamReader(System.in,StandardCharsets.UTF_8);
-        Scanner scanner = new Scanner(reader).useDelimiter("[\\p{Space}\\p{Punct}]");
+        Scanner scanner = new Scanner(reader).useDelimiter("[\\p{Space}\\p{Punct}]+");
 
         while (scanner.hasNext()) {
-            String element = scanner.next();
+            String element = scanner.next().toLowerCase(Locale.ROOT);
             int count = map.containsKey(element) ? map.get(element)+1:1;
             map.put(element,count);
         }
         scanner.close();
 
-        System.out.println(map);
+
+
+        map.entrySet().stream()
+                .sorted(Comparator.comparingInt(Map.Entry::getValue))
+                .forEach(x->System.out.println(x));
+
+
+
+
+
+
+
+
+
 
 
 
